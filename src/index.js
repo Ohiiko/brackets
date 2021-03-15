d49be0dc59998348e8f2494427cf17a8eabd776d
Pavel Ohiiko <timurid346@gmail.com>
2021-03-16 01:08:12 +0200

    fix: solution replacement

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-module.exports = function check(input, bracketsConfig, opening, closing) {
-    if (opening == undefined || closing == undefined) {
-        (opening = []), (closing = []);
-        for (let i = 0; i < bracketsConfig.length; i++) {
-            opening.push(bracketsConfig[i][0]);
-            closing.push(bracketsConfig[i][1]);
+module.exports = function check(str, bracketsConfig) {
+    let stack = []
+  
+    for(let bracket of str) {
+      let bracketsIndex = bracketsConfig.indexOf(bracket)
+  
+      if (bracketsIndex === -1){
+        continue
+      }
+  
+      if(bracketsIndex % 2 === 0) {
+        stack.push(bracketsIndex + 1)
+      } else {
+        if(stack.pop() !== bracketsIndex) {
+          return false;
         }
-    } else {
-        opening, closing;
+      }
     }
-
-    if (input.length == 0) return true;
-    let type = opening.indexOf(input.charAt(0));
-    if (type == -1) return false;
-    for (let pos = 1; pos < input.length; pos++) {
-        // forward search
-        if (closing.indexOf(input.charAt(pos)) == type) {
-            let inside = input.slice(1, pos);
-            let after = input.slice(pos + 1);
-            if (
-                check(inside, bracketsConfig, opening, closing) &&
-                check(after, bracketsConfig, opening, closing)
-            )
-                return true;
-        }
-    }
-    return false;
-};
+    return stack.length === 0
+  }
